20bd6bc97fb79e62814108981024b268c138dfad
Samuel Kendall <kinguser981@gmail.com>
2024-10-07 05:31:21 +0330

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,8 @@ AB_OTA_PARTITIONS += \
     product \
     vendor \
     system_ext
-BOARD_USES_RECOVERY_AS_BOOT := true
+
+# BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Architecture
 TARGET_ARCH := arm64
